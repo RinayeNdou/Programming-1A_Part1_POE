4643cae38d68c69a71237f7acd565b20d0d75917
Rinaye Ndou <st10470759@rcconnect.edu.za>
2026-09-02 13:26:48 +0200

    initial project setup

---
+++ src/main/java/com/st10470759/chatapp_part1/ChatApp_Part1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.st10470759.chatapp_part1;
+
+/**
+ *
+ * @author rnndo
+ */
+public class ChatApp_Part1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
