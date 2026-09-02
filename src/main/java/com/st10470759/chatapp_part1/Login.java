4f3f9fa31e7d18fd93d1ee852fd3418881840384
Rinaye Ndou <st10470759@rcconnect.edu.za>
2026-09-02 14:05:18 +0200

    Rename main class to Registration, add Login class

--- src/main/java/com/st10470759/chatapp_part1/Login.java
+++ src/main/java/com/st10470759/chatapp_part1/Login.java
@@ -1,16 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.st10470759.chatapp_part1;
 
 /**
  *
  * @author rnndo
  */
-public class Registration {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public class Login {
+    
 }
